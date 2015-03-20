b6c6443df20ed219c43125fc21e0a5c09a431168
David Seddon <david@seddonym.me>
2015-03-20 12:15:04 +0000

    Adjusted dev settings

--- settings/installations.py
+++ settings/installations.py
@@ -34,3 +34,41 @@ class Local(ProjectConfiguration):
 
 class Dev(ProjectConfiguration):
     DEBUG = True
+    DOMAIN = 'buzzhire.localhost'
+    WEBFACTION_USER = 'buzzhire'
+    WEBFACTION_APPNAME = 'dev'
+    
+    @classproperty
+    def PROJECT_ROOT(cls):
+        return '/home/%s/webapps/%s/project' % (cls.WEBFACTION_USER, WEBFACTION_APPNAME)
+
+    MANAGERS = ADMINS = (
+        ('David Seddon', 'david@seddonym.me'),
+    )
+
+    @classproperty
+    def BASE_URL(cls):
+        return '%s://%s' % (cls.PROTOCOL, cls.DOMAIN)
+
+    ACCOUNT_PASSWORD_MIN_LENGTH = 1
+
+    @classmethod
+    def get_static_root(cls):
+        return ''
+
+    @classproperty
+    def STATICFILES_DIRS(cls):
+        return (os.path.join(cls.get_setting('PROJECT_ROOT'), 'static'),)
+
+    @classproperty
+    def STATIC_ROOT(cls):
+        return '/home/%s/webapps/%s_static/' % (cls.WEBFACTION_USER, cls.WEBFACTION_APPNAME)
+
+    @classproperty
+    def MEDIA_ROOT(cls):
+        return '/home/%s/webapps/%s_uploads/' % (cls.WEBFACTION_USER, cls.WEBFACTION_APPNAME)
+
+    @classproperty
+    def LOG_PATH(cls):
+        return '/home/%s/logs/user/%s/' % (cls.WEBFACTION_USER, cls.WEBFACTION_APPNAME)
+
