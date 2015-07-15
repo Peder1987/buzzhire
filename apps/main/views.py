39657cbb6563d2f7ffe835a7c1e151287fb9234a
David Seddon <david@seddonym.me>
2015-07-15 14:29:26 +0100

    Added redirect to account dashboard away from home page for all logged in users

--- apps/main/views.py
+++ apps/main/views.py
@@ -2,6 +2,8 @@ from django.core.exceptions import PermissionDenied
 from django.views.generic.base import TemplateView
 from apps.core.views import ContextTemplateView, ContextMixin
 from apps.service.forms import ServiceSelectForm
+from django.shortcuts import redirect
+
 
 class TestError(TemplateView):
     "View used to trigger site error, for testing purposes."
@@ -18,6 +20,13 @@ class TestDenied(TemplateView):
 class HomeView(TemplateView):
     "The home page."
     template_name = 'main/home.html'
+
+    def dispatch(self, *args, **kwargs):
+        # Redirect to the dashboard, if the user is logged in
+        if self.request.user.is_authenticated():
+            return redirect('account_dashboard')
+
+        return super(HomeView, self).dispatch(*args, **kwargs)
 
     def get_context_data(self, *args, **kwargs):
         context = super(HomeView, self).get_context_data(*args, **kwargs)
