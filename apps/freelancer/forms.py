87b4414cf80bab1f5d166a3a2abb7e2d2c4a33fb
Jovana <jovana.stefanovic.js@gmail.com>
2015-07-24 20:19:17 +0200

    palceholders

--- apps/freelancer/forms.py
+++ apps/freelancer/forms.py
@@ -66,6 +66,7 @@ class FreelancerForm(CrispyFormMixin, PostcodeFormMixin, forms.ModelForm):
             self.fields['raw_postcode'].initial = str(self.instance.postcode)
 
         self.fields['first_name'].label = False
+        self.fields['first_name'].placeholder = "First name"
 
         self.helper.layout = layout.Layout(
             layout.Fieldset(
