a37cdc0bdc582a01571f8d9cc1dabc4e7d02f814
David Seddon <david@seddonym.me>
2015-08-24 14:48:12 +0100

    Changed user picker to raw id field in admin

--- apps/client/admin.py
+++ apps/client/admin.py
@@ -9,7 +9,7 @@ class LeadAdmin(admin.ModelAdmin):
 class ClientAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'user',
                     'company_name', 'mobile')
-
+    raw_id_fields = ('user',)
 
 admin.site.register(models.Lead, LeadAdmin)
 admin.site.register(models.Client, ClientAdmin)
