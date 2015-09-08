fc1fd5e6d3205d86b694d18b27ab538f2c78e4aa
David Seddon <david@seddonym.me>
2015-09-08 15:18:55 +0100

    Added client export

--- apps/client/admin.py
+++ apps/client/admin.py
@@ -1,17 +1,45 @@
 from django.contrib import admin
+from import_export.admin import ExportActionModelAdmin
+from import_export import resources, fields
 from . import models
 
 
 class LeadAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email', 'created')
 
+class ClientResource(resources.ModelResource):
+    "Import/export resource to allow exporting of client data."
+    reference_number = fields.Field(column_name='Client reference')
+    def dehydrate_reference_number(self, obj):
+        return obj.reference_number
 
-class ClientAdmin(admin.ModelAdmin):
+    last_name = fields.Field(column_name='Last name',
+                                attribute='last_name')
+    first_name = fields.Field(column_name='First name',
+                                attribute='first_name')
+    company_name = fields.Field(column_name='Company name',
+                                attribute='company_name')
+    mobile = fields.Field(column_name='Mobile',
+                                attribute='mobile')
+    user__email = fields.Field(column_name='Email',
+                                attribute='user__email')
+
+    class Meta:
+        model = models.Client
+        fields = ('reference_number',
+                  'last_name', 'first_name',
+                  'company_name', 'mobile', 'user__email')
+        export_order = fields
+
+
+class ClientAdmin(ExportActionModelAdmin):
     list_display = ('first_name', 'last_name', 'user',
                     'company_name', 'mobile')
     raw_id_fields = ('user',)
     search_fields = ('first_name', 'last_name',
                      'user__email')
+    resource_class = ClientResource
+
 
 admin.site.register(models.Lead, LeadAdmin)
 admin.site.register(models.Client, ClientAdmin)
